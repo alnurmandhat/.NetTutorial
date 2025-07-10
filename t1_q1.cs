4e730d13a6ec3e26284d719bd4ae4c758058fc7a
Atik Mandhat <atikmandhat5@gmail.com>
2025-07-10 15:07:52 +0530

    Add project files.

---
+++ t1_q1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1
+{
+    internal class t1_q1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("24SOECE13034_MANDHAT ALNUR");
+            Console.WriteLine("Hello World");
+        }
+
+    }
+}
