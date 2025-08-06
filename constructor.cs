8ebadfd7f06a4c671f248759a5e37c05e79c34ad
Atik Mandhat <atikmandhat5@gmail.com>
2025-08-06 11:35:56 +0530

    sir task

---
+++ constructor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1
+{
+    internal class constructor
+    {
+        class employee
+        {
+            int emailId;
+            string name, city;
+            public employee()
+            {
+                Console.WriteLine("heloo");
+            }
+            public employee(int emailId, string name)
+            {
+                this.emailId = emailId;
+                this.name = name;
+
+            }
+            public employee(int emailId, string name, string city) : this(emailId, name)
+            {
+
+                this.city = city;
+            }
+            public void display()
+            {
+                Console.WriteLine("email ID " + emailId);
+                Console.WriteLine("name  " + name);
+                Console.WriteLine("City " + city);
+
+            }
+        }
+        public static void Main(string[] args)
+        {
+            employee obj1 = new employee();
+            employee obj2 = new employee(101,"Noor","Rajkot");
+            obj2.display();
+        }
+    }
+}
---
+++ method_overloading.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1
+{
+    internal class method_overloading
+    {
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+        static int add(int a , int b, int c)
+        {
+            return a + b + c;
+        }
+        static float add(float a, float b)
+        {
+            return a + b;
+        }
+        static float add(float a, float b, float c)
+        {
+            return a + b + c;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Answer of a & b is " + add(2, 1));
+            Console.WriteLine("Answer of a & b is " + add(2.4f, 1.5f));
+            Console.WriteLine("Answer of a b & c is "+add(3, 2 , 2));
+            Console.WriteLine("Answer of a & b is " + add(2.4f, 1.5f, 3.2f));
+        }
+
+    }
+}
